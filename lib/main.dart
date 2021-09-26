ca75e27b3a2b21b52e3a11d4fe9118daf8647b37
Shiv <sky.shivy@gmail.com>
2021-09-26 08:49:48 +0530

    news

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'HomeScreen/home_screen.dart';
 
 void main() {
+  // main app test commit
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "First App",
