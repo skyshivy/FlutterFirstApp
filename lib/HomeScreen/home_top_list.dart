7b967db6c3ec16ebb37b1fb116c278d2b812979e
Shiv <sky.shivy@gmail.com>
2021-09-27 19:11:59 +0530

    custom corner radius

--- lib/HomeScreen/home_top_list.dart
+++ lib/HomeScreen/home_top_list.dart
@@ -59,7 +59,10 @@ class ImageContainer extends StatelessWidget {
       //borderRadius: BorderRadius.circular(300.0),
 
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(10.0),
+        borderRadius: BorderRadius.only(
+          topLeft: const Radius.circular(10.0),
+          topRight: const Radius.circular(10.0),
+        ),
         child: Image.network(
           "https://picsum.photos/id/1/200",
           fit: BoxFit.cover,
