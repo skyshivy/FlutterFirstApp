f4da160bf615078e6790daeebca356c88d2d95af
Shiv <sky.shivy@gmail.com>
2021-09-28 08:45:38 +0530

    new change

--- lib/HomeScreen/home_category.dart
+++ lib/HomeScreen/home_category.dart
@@ -6,29 +6,29 @@ class HomeCategory extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-      padding: EdgeInsets.all(15),
+      padding: EdgeInsets.all(20),
       height: 100 * items,
       child: GridView.builder(
         itemCount: 6,
         physics: NeverScrollableScrollPhysics(),
         scrollDirection: Axis.vertical,
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
         itemBuilder: (context, index) {
           return Container(
-              padding: EdgeInsets.all(5),
+              //padding: EdgeInsets.all(5),
               child: GestureDetector(
-                onTap: () {
-                  print("category tapped $index");
-                },
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Colors.pink,
-                  ),
-                ),
-              ));
+            onTap: () {
+              print("category tapped $index");
+            },
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.pink,
+              ),
+            ),
+          ));
         },
       ),
     );
